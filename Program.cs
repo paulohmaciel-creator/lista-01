e4414cae3fa1845c4da03fcc5aa5ecca9f34da21
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-11 20:16:48 -0300

    questao 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("idade do visitante");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 16)
+{
+    Console.WriteLine("Entrada liberada somente com responsável");
+}
+else
+{
+    Console.WriteLine("Entrada liberada");
+}
