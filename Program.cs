f746fbdc5e65b94fe131cbba3a58b99d9472031e
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-15 18:58:06 -0300

    questao2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿Console.WriteLine("idade do visitante");
-int idade = int.Parse(Console.ReadLine());
-
-if (idade < 16)
+﻿Console.WriteLine("informe anota final do estudante");
+if (double.TryParse(Console.ReadLine(), out double notaFinal))
 {
-    Console.WriteLine("Entrada liberada somente com responsável");
+    if (notaFinal >= 7.0)
+    {
+        Console.WriteLine("Aprovado");
+    }
+    else if (notaFinal >= 5.0)
+    {
+        Console.WriteLine("Recuperação");
+    }
+    else
+    {
+        Console.WriteLine("Reprovado");
+    }
 }
 else
 {
-    Console.WriteLine("Entrada liberada");
+    Console.WriteLine("Valor inválido. Por favor, insira um número.");
 }
