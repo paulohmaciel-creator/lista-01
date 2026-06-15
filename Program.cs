60a95fcf911d12a99e31d42bf9b6756365a4819b
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-15 19:34:22 -0300

    questao4

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-﻿Console.WriteLine("temperatura atual em graus celsius: "); 
-float temperatura = float.Parse(Console.ReadLine());
-if (temperatura < 18) 
-{ 
-    Console.WriteLine("ambiente frio"); 
-} 
-else if (temperatura >= 18 && temperatura <= 26) 
-{ 
-    Console.WriteLine("ambiente confortável."); 
-} 
-else 
-{ 
-    Console.WriteLine("ambiente quente"); 
+﻿Console.Write("Informe o valor total da compra: ");
+decimal ValorFinal = decimal.Parse(Console.ReadLine());
+
+decimal desconto = 0;
+decimal valorFinal = 0;
+
+
+if (ValorFinal >= 200)
+{
+    
+    desconto = ValorFinal * 0.10m;
 }
+else
+{
+    
+    desconto = 0;
+}
+
+valorFinal = ValorFinal - desconto;
+
+Console.WriteLine(); 
+
+Console.WriteLine("Valor original: R$ " + ValorFinal);
+Console.WriteLine("Desconto aplicado: R$ " + desconto);
+Console.WriteLine("Valor final: R$ " + valorFinal);
