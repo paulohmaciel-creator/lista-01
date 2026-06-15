3f65585f4fe5bd030c07339a3a48e8aabe46a22b
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-15 20:41:16 -0300

    questao9

--- Program.cs
+++ Program.cs
@@ -1,33 +1,8 @@
-﻿using System;
+﻿ Console.Write("Informe um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
 
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Informe a primeira pontuação: ");
-        int a = int.Parse(Console.ReadLine());
-
-        Console.Write("Informe a segunda pontuação: ");
-        int b = int.Parse(Console.ReadLine());
-
-        Console.Write("Informe a terceira pontuação: ");
-        int c = int.Parse(Console.ReadLine());
-
-        if (a == b && b == c)
-        {
-            Console.WriteLine("As três pontuações são iguais.");
-        }
+        if (num % 2 == 0)
+            Console.WriteLine("O número informado é par.");
         else
-        {
-            int maior = a;
-
-            if (b > maior)
-                maior = b;
-
-            if (c > maior)
-                maior = c;
-
-            Console.WriteLine("Maior pontuação: " + maior);
-        }
-    }
-}
+            Console.WriteLine("O número informado é ímpar.");
+    
