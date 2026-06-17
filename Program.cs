46a848ee4584d79df927b310496a873bd07595e3
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-17 20:58:01 -0300

    questao20

--- Program.cs
+++ Program.cs
@@ -1,35 +1,39 @@
-﻿Console.Write("Informe o saldo inicial: ");
-double saldo = double.Parse(Console.ReadLine());
+﻿Console.Write("Informe a quantidade inicial em estoque: ");
+int estoque = int.Parse(Console.ReadLine());
 
 int operacao = 0;
 
-while (operacao != 3)
+while (operacao != 4)
 {
     Console.Write("Escolha a operação: ");
     operacao = int.Parse(Console.ReadLine());
 
     if (operacao == 1)
     {
-        Console.Write("Informe o valor da movimentação: ");
-        double valor = double.Parse(Console.ReadLine());
-        saldo = saldo + valor;
-        Console.WriteLine("Entrada registrada. Saldo atual: R$ " + saldo);
+        Console.Write("Informe a quantidade movimentada: ");
+        int quantidade = int.Parse(Console.ReadLine());
+        estoque = estoque + quantidade;
+        Console.WriteLine("Entrada registrada. Estoque atual: " + estoque);
     }
     else if (operacao == 2)
     {
-        Console.Write("Informe o valor da movimentação: ");
-        double valor = double.Parse(Console.ReadLine());
+        Console.Write("Informe a quantidade movimentada: ");
+        int quantidade = int.Parse(Console.ReadLine());
 
-        if (valor <= saldo)
+        if (quantidade <= estoque)
         {
-            saldo = saldo - valor;
-            Console.WriteLine("Saída registrada. Saldo atual: R$ " + saldo);
+            estoque = estoque - quantidade;
+            Console.WriteLine("Saída registrada. Estoque atual: " + estoque);
         }
         else
         {
-            Console.WriteLine("Saldo insuficiente.");
+            Console.WriteLine("Quantidade insuficiente em estoque.");
         }
+    }
+    else if (operacao == 3)
+    {
+        Console.WriteLine("Estoque atual: " + estoque);
     }
 }
 
-Console.WriteLine("Saldo final: R$ " + saldo);
+Console.WriteLine("Estoque final: " + estoque);
